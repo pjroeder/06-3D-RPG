2f1532f3c285bf47e590b55bc2de32486d6a5673
pjroeder <pjroeder@iu.edu>
2021-12-11 19:43:43 -0500

    finished

--- Global.gd
+++ Global.gd
@@ -1,7 +1,15 @@
 extends Node
 
-var timer = 0
+var time = 0
 var score = 0
+
+func _ready():
+	update_score(0)
+
+func reset():
+	time = 0
+	score = 0
+	update_score(0)
 
 func _input(_event):
 	if Input.is_action_pressed("menu"):
@@ -12,8 +20,8 @@ func update_score(s):
 	get_node("/root/Game/UI/Score").text = "Score " + str(score)
 
 func update_time():
-	var t_m = floor(timer / 60.0)
-	var t_s = timer % 60 
+	var t_m = floor(time / 60.0)
+	var t_s = time % 60 
 	var t = "Time: %02d" % t_m
 	t += ":%02d" % t_s
 	get_node("/root/Game/UI/Time").text = t
